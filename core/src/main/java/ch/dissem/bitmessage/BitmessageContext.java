86ed0d0b7fec3d891040d71a6792cdc2d8f533b4
Seb Nap <sebnapi@gmail.com>
2016-08-12 20:44:57 +0200

    Removed NonNull-check to let the if-block catch it

--- core/src/main/java/ch/dissem/bitmessage/BitmessageContext.java
+++ core/src/main/java/ch/dissem/bitmessage/BitmessageContext.java
@@ -393,7 +393,6 @@ public class BitmessageContext {
             nonNull("networkHandler", networkHandler);
             nonNull("addressRepo", addressRepo);
             nonNull("messageRepo", messageRepo);
-            nonNull("proofOfWorkRepo", proofOfWorkRepository);
             if (proofOfWorkEngine == null) {
                 proofOfWorkEngine = new MultiThreadedPOWEngine();
             }
